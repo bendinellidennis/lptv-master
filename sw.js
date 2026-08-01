91d7f519e60e93fbeefc9fca15dac067c2deaf31
bendinellidennis <bendinellidennis@gmail.com>
2026-08-01 22:41:22 +0200

    Build 03 stabile

--- sw.js
+++ sw.js
@@ -1,11 +1,1 @@
-const CACHE='lptv-stable-1';
-const FILES=['./','./index.html','./database.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([
-  self.clients.claim(),
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-])));
-self.addEventListener('fetch',e=>e.respondWith(
-  fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r})
-  .catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
-));
+const CACHE='mdm-build-03';const ASSETS=['./','./index.html','./styles.css','./app.js','./database.js','./content.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});

c0d0c28917b17c479c83fb493c689053583e8be2
bendinellidennis <bendinellidennis@gmail.com>
2026-08-03 00:02:04 +0200

    Mod errore zero

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE='mdm-build-20';
+const CACHE='mdm-build-21';
 const ASSETS=[
  './',
  './index.html',
- './styles.css?v=20.0',
- './app.js?v=20.0',
- './database.js?v=20.0',
- './content.js?v=20.0',
+ './styles.css?v=21.0',
+ './app.js?v=21.0',
+ './database.js?v=21.0',
+ './content.js?v=21.0',
  './manifest.webmanifest',
  './icon-192.png',
  './icon-512.png'

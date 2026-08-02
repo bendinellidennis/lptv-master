a9ccd120b79485c5c3edc3591b5d2c0fbd267fd1
bendinellidennis <bendinellidennis@gmail.com>
2026-08-02 18:17:31 +0200

    Build 15 stabile - Dashboard scuola

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE='mdm-build-14';
+const CACHE='mdm-build-15';
 const ASSETS=[
  './',
  './index.html',
- './styles.css?v=14.0',
- './app.js?v=14.0',
- './database.js?v=14.0',
- './content.js?v=14.0',
+ './styles.css?v=15.0',
+ './app.js?v=15.0',
+ './database.js?v=15.0',
+ './content.js?v=15.0',
  './manifest.webmanifest',
  './icon-192.png',
  './icon-512.png'
